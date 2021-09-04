b1021dccaba50624d7094e9809d586905cee6ca8
cl1v3287 <74298593+cl1v3287@users.noreply.github.com>
2021-09-04 13:59:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const canvas =document.getElementById("mycanvas")
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+
+const ctx = canvas.getContext("2d")
+
+const balls = []
+
+for(let i = 0;i<18;i++){
+    let r = Math.floor(Math.random()*35)+18
+    let x = Math.random()*(canvas.width-r*2)+r
+    let y = Math.random()*(canvas.height-r*2)+r
+    let c = '#' + (function co(lor){   return (lor +=
+        [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+        && (lor.length == 6) ?  lor : co(lor); })('');
+
+    balls.push(new Circle(x,y,r,c,))
+}
+
+function Circle(x,y,r,c){
+    this.x=x
+    this.y=y
+    this.r=r
+    this.c=c
+    this.dx = Math.floor(Math.random()*4)+1
+    this.dx *= Math.floor(Math.random()*2)==1?1:-1
+    this.dy = Math.floor(Math.random()*4)+1
+    this.dy *= Math.floor(Math.random()*2)==1?1:-1
+
+    this.draw=function(){
+        ctx.beginPath()
+        ctx.fillStyle= this.c
+        ctx.arc(this.x,this.y,this.r,0,Math.PI*2)
+        ctx.fill()
+    }
+    this.animate=function(){
+        this.x+=this.dx
+        this.y+=this.dy
+
+        if(this.x+this.r>canvas.width||this.x-this.r<0){
+            this.dx=-this.dx
+        }
+        if(this.y+this.r>canvas.height||this.y-this.r<0){
+            this.dy=-this.dy
+        }
+        this.draw()
+    }
+}
+
+function update(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    for(let i = 0;i<balls.length;i++){
+        let ball = balls[i]
+        ball.animate()
+    }
+    requestAnimationFrame(update)
+}
+update()
